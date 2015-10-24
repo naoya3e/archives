d8838ad17b2f6537ad49cb01e97cbb343e856a53
nazna <14846330+nazna@users.noreply.github.com>
2015-10-24 16:46:42 +0900

    [add] 指定したフライトデータ出力プログラム

--- Practice/c/FlightSchedule/flight.c
+++ Practice/c/FlightSchedule/flight.c
@@ -7,53 +7,65 @@ typedef struct _FLIGHT {
   char from[10];     // 出発地
   char to[10];       // 到着地
   int num;           // 便名
-  int d_hour;        // 出発時刻 時
-  int d_min;         // 出発時刻 分
-  int a_hour;        // 到着時刻 時
-  int a_min;         // 到着時刻 分
+  int d_hour;        // 出発時刻 hh
+  int d_min;         // 出発時刻 mm
+  int a_hour;        // 到着時刻 hh
+  int a_min;         // 到着時刻 mm
 } FLIGHT;
 
-int read_airline(char *filename[], int n, FLIGHT airline[]) {
-  int i = 0;
-  char carrier[10];
-  char from[10];
-  char to[10];
+// フライトデータ全取得
+int read_airline(char *files[], int n, FLIGHT airline[]) {
+  int i, j = 0;
+  char carrier[10];  // 航空会社
+  char from[10];  // 出発地
+  char to[10];  // 到着地
   FILE *fp;
 
-  if ((fp = fopen(filename, "r")) == NULL) {
-    fprintf(stderr, "ERROR: Cannot read file.\n");
-    exit(EXIT_FAILURE);
+  for (i=0; i<n; i++) {
+    // ARGVは0番目に実行ファイル名が入っているので読み飛ばし
+    if ((fp = fopen(files[i+1], "r")) == NULL) {
+      fprintf(stderr, "ERROR: Cannot read file.\n");
+      exit(EXIT_FAILURE);
+    }
+
+    fscanf(fp, "%s", carrier);  // ファイルから航空会社を読み取り
+    fscanf(fp, "%s%s", from, to);  // ファイルから出発地と到着地を読み取り
+
+    while (fscanf(fp, "%d,%d:%d,%d:%d", &airline[j].num, &airline[j].d_hour, &airline[j].d_min, &airline[j].a_hour, &airline[j].a_min) != EOF) {
+      strcpy(airline[j].carrier, carrier);
+      strcpy(airline[j].from, from);
+      strcpy(airline[j].to, to);
+      j++;
+    }
+
+    fclose(fp);
   }
 
-  fscanf(fp, "%s", carrier);
-  fscanf(fp, "%s%s", from, to);
-
-  while (fscanf(fp, "%d,%d:%d,%d:%d", &airline[i].num, &airline[i].d_hour, &airline[i].d_min, &airline[i].a_hour, &airline[i].a_min) != EOF) {
-    strcpy(airline[i].carrier, carrier);
-    strcpy(airline[i].from, from);
-    strcpy(airline[i].to, to);
-    i++;
-  }
-
-  fclose(fp);
-
-  return i;
+  return j;
 }
 
-FLIGHT *search_flight(FLIGHT airline[], int hour, int min) {
-  int i = 0;
-  int dif_hour = 24, dif_min = 60;
-  int rel_hour, rel_min;
+// 該当フライトデータ取得
+FLIGHT *search_flight(FLIGHT airline[], int hour, int min, int dep, int arr) {
+  int i;
+  int status;  // 出発地と到着地の整合性状況
+  int ref_time;  // 出発時刻を分単位へ変換
+  int input_time;  // 入力時間を分単位へ変換
+  int dif_time = 1500;  // 指定時間と出発時間の最近の差
+  char departure[10];  // 引数に従って文字列変換
+  char arrival[10];  // 引数に従って文字列変換
   FLIGHT *target = NULL;
 
-  for (i=0; i<sizeof(airline)-2; i++) {
-    printf("now_hour:%d  now_min:%d\n", airline[i].d_hour, airline[i].d_min);
-    rel_hour = airline[i].d_hour - hour;
-    rel_min  = airline[i].d_min - min;
+  strcpy(departure, (dep == 1)? "岡山": "羽田");
+  strcpy(arrival, (arr == 1)? "岡山": "羽田");
 
-    if (rel_hour >= 0 && rel_min >= 0 && dif_hour > rel_hour && dif_min > rel_min) {
-      dif_hour = rel_hour;
-      dif_min = rel_min;
+  input_time = 60*hour + min;
+
+  for (i=0; i<airline[i].num!=0; i++) {
+    ref_time = 60*airline[i].d_hour + airline[i].d_min;
+    status = strcmp(airline[i].from, departure) || strcmp(airline[i].to, arrival);
+
+    if (ref_time >= input_time && dif_time > ref_time - input_time && status == 0) {
+      dif_time = ref_time - input_time;
       target = &airline[i];
     }
   }
@@ -61,28 +73,24 @@ FLIGHT *search_flight(FLIGHT airline[], int hour, int min) {
   return target;
 }
 
-void print_flight(FLIGHT *flight) {
+// 該当フライトデータ表示
+void print_flight(FLIGHT *flight, int n) {
   if (flight == NULL) {
-    printf("フライトがありませんでした.\n");
+    if (n == 0) printf("フライトがありませんでした.\n");
   } else {
-    printf("%s%4d便 %s %02d:%02d発 %s %02d:%02d着\n", flight->carrier, flight->num, flight->from, flight->d_hour, flight->d_min, flight->to, flight->a_hour, flight->a_min);
+    printf("%2d.  %s %4d便 %s %02d:%02d発 %s %02d:%02d着\n", n+1, flight->carrier, flight->num, flight->from, flight->d_hour, flight->d_min, flight->to, flight->a_hour, flight->a_min);
   }
 }
 
 int main(int argc, char *argv[]) {
   int i;
-  int flights;
-  int hour, min;
-  int dep, arr, n;
-  char *files[10];
-  FLIGHT airline[10];
-  FLIGHT *line;
+  int flights;  // フライト数
+  int hour, min;  // 指定時刻 hh:mm
+  int dep, arr, n;  // 出発地, 到着地, 表示数
+  FLIGHT *line;  // 該当フライト参照ポインタ
+  FLIGHT airline[40] = {0};  // フライト数が40までなら格納できる
 
-  for (i=0; i<argc; i++) {
-    strcpy(files[i], argv[i+1]);
-  }
-
-  flights = read_airline(files, argc-1, airline);
+  flights = read_airline(argv, argc-1, airline);
 
   printf("時刻を(hh:mm)の形式で入力してください >> ");
   scanf("%d:%d", &hour, &min);
@@ -93,13 +101,18 @@ int main(int argc, char *argv[]) {
   printf("表示数を入力してください [1-10] >> ");
   scanf("%d", &n);
 
-  if (dep != 1 || dep != 2 || arr != 1 || arr != 2 || n < 1 || n > 10) {
-    fprintf(stderr, "ERROR: Unexpected value was inputted.");
+  if (dep == arr || n < 1 || n > 10) {
+    fprintf(stderr, "ERROR: Unexpected value was inputted.\n");
     exit(EXIT_FAILURE);
   }
 
-  line = search_flight(airline, hour, min);
-  print_flight(line);
+  for (i=0; i<n; i++) {
+    line = search_flight(airline, hour, min, dep, arr);
+    print_flight(line, i);
+    if (line == NULL) break;
+    hour = line->d_hour;
+    min  = line->d_min  + 1;
+  }
 
   return 0;
 }
