164f0137dde96685729a44b16e0f18564e085008
nazna <14846330+nazna@users.noreply.github.com>
2016-02-07 00:26:11 +0900

    [update] 条件は1111の倍数かどうかを見るだけでよかった

--- AtCoder/ABC_033/A_暗証番号.cpp
+++ AtCoder/ABC_033/A_暗証番号.cpp
@@ -1,23 +1,13 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
-
-#define REP(i, n) for(int i=0; i<n; i++)
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
 
-  string str;
+  string s;
+  cin >> s;
 
-  cin >> str;
-
-  if (str[0] == str[1] && str[1] == str[2] && str[2] == str[3] && str[3] == str[0]) {
-    cout << "SAME" << endl;
-  } else {
-    cout << "DIFFERENT" << endl;
-  }
+  if (stoi(s)%1111 == 0) cout << "SAME" << endl;
+  else cout << "DIFFERENT" << endl;
 }
 
