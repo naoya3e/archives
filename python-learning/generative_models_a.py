093678b4aa2e3b8fad7aa36a3abe0dbe0d1cc83d
nazna <14846330+nazna@users.noreply.github.com>
2017-06-15 17:31:58 +0900

    update: argparseによって状態Zの生成数を制御するように更新

--- python-learning/generative_models_a.py
+++ python-learning/generative_models_a.py
@@ -1,20 +1,25 @@
-from scipy import stats
-
+import argparse
 import numpy as np
 
+from scipy import stats
+
+parser = argparse.ArgumentParser(description='Generative model A')
+parser.add_argument('-n', type=int, required=True)
+args = parser.parse_args()
+
 theta = [0.1, 0.3, 0.1, 0.2, 0.2, 0.1]
-l = len(theta)
 
 def normalize(xs):
     return [float(i)/sum(xs) for i in xs]
 
-def categorical(l, theta):
-    custm = stats.rv_discrete(name='custm', values=(np.arange(l), theta))
-    return custm.rvs(size=l)
+def categorical(theta, n=1):
+    custm = stats.rv_discrete(name='custm', values=(np.arange(len(theta)), theta))
+    return custm.rvs(size=n)
 
-z = categorical(l, theta)
-x = categorical(l, normalize(z))
+z = categorical(theta, args.n)
+x = categorical(normalize(z))[0]
 
-print('theta: ', theta)
-print('z    : ', z)
-print('x    : ', x)
+print('theta     : ', theta)
+print('z         : ', z)
+print('normalized: ', normalize(z))
+print('x         : ', x)
--- python-learning/generative_models_b.py
+++ python-learning/generative_models_b.py
@@ -1,23 +1,28 @@
-from scipy.stats import dirichlet
-from scipy import stats
-
+import argparse
 import numpy as np
 
+from scipy import stats
+from scipy.stats import dirichlet
+
+parser = argparse.ArgumentParser(description='Generative model B')
+parser.add_argument('-n', type=int, required=True)
+args = parser.parse_args()
+
 alpha = [1, 3, 8, 2, 6, 2]
-l = len(alpha)
 
 def normalize(xs):
     return [float(i)/sum(xs) for i in xs]
 
-def categorical(l, theta):
-    custm = stats.rv_discrete(name='custm', values=(np.arange(l), theta))
-    return custm.rvs(size=l)
+def categorical(theta, n=1):
+    custm = stats.rv_discrete(name='custm', values=(np.arange(len(theta)), theta))
+    return custm.rvs(size=n)
 
 theta = dirichlet.rvs(alpha).tolist()[0]
-z = categorical(l, theta)
-x = categorical(l, normalize(z))
+z = categorical(theta, args.n)
+x = categorical(normalize(z))[0]
 
-print('alpha: ', alpha)
-print('theta: ', theta)
-print('z    : ', z)
-print('x    : ', x)
+print('alpha     : ', alpha)
+print('theta     : ', theta)
+print('z         : ', z)
+print('normalized: ', normalize(z))
+print('x         : ', x)
