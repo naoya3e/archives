c6050fc09bb51f6bf818d33d27b232c79871ff30
nazna <14846330+nazna@users.noreply.github.com>
2019-02-17 22:55:51 +0900

    add: LeetCode_021 2つのソート済みな単方向連結リストをソートして結合 headとcurrentの2つを持っていることが重要

---
+++ leetcode/021_merge_two_sorted_list.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: 'ListNode', l2: 'ListNode') -> 'ListNode':
+        head = current = ListNode(0)
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        current.next = l1 or l2
+
+        return head.next
